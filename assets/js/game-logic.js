7908726839c78ac6180ef04629c9a35ed40ca08b
dudeguythethird <keablejoseph@gmail.com>
2020-12-30 10:52:35 +0000

    implements buystuffgetter function in its most basic form

--- assets/js/game-logic.js
+++ assets/js/game-logic.js
@@ -1,12 +1,26 @@
 $(document).ready(function () {
 
     var score = 0;
+    var stuffGetterCost = 15;
+    var stuffGetters = 0;
 
-    $(".get-stuff").click(addToScore(1));
-
-    function addToScore(amount) {
-        score = score + amount;
+    function addToScore() {
+        score++;
         //document.getElementById("total-stuff-amount").innerHTML = score;
         $("#total-stuff-amount").text(score);
     }
+
+    function buyStuffGetter() {
+        if (score >= stuffGetterCost) {
+            score = score - stuffGetterCost;
+            stuffGetters = stuffGetters + 1;
+            stuffGetterCost = Math.round(stuffGetterCost * 1.15);
+            $("#total-stuff-amount").text(score);
+            $("#auto-cost").text(stuffGetterCost);
+            $("#sps-value").text(stuffGetters);
+        }
+    }
+    
+    $(".get-stuff").click(addToScore);
+    $("#auto-buy").click(buyStuffGetter);
 })
