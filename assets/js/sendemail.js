f9e3311366bab5c009c47d72f3170e08f8c2d281
dudeguythethird <keablejoseph@gmail.com>
2021-01-05 16:01:39 +0000

    imlements a functional form and a footer with social media links

---
+++ assets/js/sendemail.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "template_clicker", {
+        "from_name":contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "idea_suggestion": contactForm.ideasuggestion.value
+    })
+    .then(
+        function(response){
+            console.log("SUCCESS", response);
+            alert("Idea Submitted!");
+        },
+        function(error){
+            console.log("FAILED", error);
+            alert("Error submitting idea, please try again later")
+        }
+    );
+    return false;
+}
